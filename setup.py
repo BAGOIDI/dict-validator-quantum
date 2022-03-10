7e7130a2c8d712d94c3f32275e8b683c77923eaf
bagoidi abel <abel.kofisallc@gmail.com>
2022-03-10 20:05:04 +0100

    readme

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='dict_validator_quantum',
+    name='dict_validator_q',
     version= "0.0.1",
 
     url='https://github.com/BAGOIDI/dict_validator_quantum.git',
     author='BAGOIDI',
     author_email='abel.kofisallc@gmail.com',
 
-    packages=['dict_validator_quantum'],
+    packages=['dict_validator_q'],
 )
