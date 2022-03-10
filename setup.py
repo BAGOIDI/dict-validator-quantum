fa6750fbe6afe4c7bd2ea0a832cc5c74769c7063
bagoidi abel <abel.kofisallc@gmail.com>
2022-03-10 13:47:34 +0100

    initial c

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dict_validator_quantum',
+    version= "1.0.0",
+
+    url='https://github.com/BAGOIDI/dict-validator-quantum.git',
+    author='BAGOIDI',
+    author_email='abel.kofisallc@gmail.com',
+
+    py_modules=['dict_validator_quantum'],
+)
